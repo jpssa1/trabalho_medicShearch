b38ddc7fce280880ecbd13eeaf0b93322dffd353
joao p t andrade <jpssa1@hotmail.com>
2024-10-04 01:34:22 -0300

    calendario

---
+++ lib/telas/calendario/calendario_page.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:medic_app/componentes/event.dart';
+import 'package:medic_app/cores/color.dart';
+import 'package:medic_app/telas/remedios/remedios_page.dart';
+import 'package:medic_app/widgets/app_bar_standart.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarioPage extends StatefulWidget {
+  const CalendarioPage({super.key});
+
+  @override
+  State<CalendarioPage> createState() => _CalendarioPageState();
+}
+
+class _CalendarioPageState extends State<CalendarioPage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  int selectedIndex = 2;
+
+  final List<Widget> pages = [
+    const RemediosPage(), // Corpo da página de Remédios
+    const Placeholder(), // Placeholder para Home (você pode substituir pela sua tela Home)
+    const CalendarioPage(), // Página de Calendário
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => pages[index],
+        ));
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _selectedEvent.value = _getEventsForDay(selectedDay);
+      });
+    }
+  }
+
+  Map<DateTime, List<Event>> events = {};
+  final TextEditingController _eventController = TextEditingController();
+  late final ValueNotifier<List<Event>> _selectedEvent;
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvent = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Cor.verdeLodo,
+      appBar: AppBarStandart(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Cor.bgverde,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  scrollable: true,
+                  title: Text('Lembrete'),
+                  content: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: TextField(
+                      controller: _eventController,
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          events.addAll({
+                            _selectedDay!: [Event(title: _eventController.text)]
+                          });
+                          Navigator.of(context).pop();
+                          _selectedEvent.value =
+                              _getEventsForDay(_selectedDay!);
+                          _eventController.clear();
+                        },
+                        child: Text(
+                          'Salvar',
+                          style: TextStyle(color: Cor.preto),
+                        ))
+                  ],
+                );
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Cor.verdeNeo,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              'Dia selecionado : ' + _focusedDay.toString().split(' ')[0],
+              style: TextStyle(color: Cor.branco, fontSize: 20),
+            ),
+            Card(
+              elevation: 10,
+              child: TableCalendar(
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                ),
+                availableGestures: AvailableGestures.all,
+                rowHeight: 60,
+                focusedDay: _focusedDay,
+                firstDay: DateTime.utc(2020, 10, 16),
+                lastDay: DateTime.utc(2040, 10, 16),
+                selectedDayPredicate: (day) => isSameDay(day, _focusedDay),
+                onDaySelected: _onDaySelected,
+                eventLoader: _getEventsForDay,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                  valueListenable: _selectedEvent,
+                  builder: (context, value, _) {
+                    return ListView.builder(
+                      itemCount: value.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            onTap: () => print(""),
+                            title: Text(value[index].title),
+                          ),
+                        );
+                      },
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Cor.marrom,
+          iconSize: 25,
+          selectedItemColor: Cor.verdeIcones,
+          elevation: 5,
+          currentIndex: selectedIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_hospital),
+              label: 'Medicacoes',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: 'Calendario',
+            ),
+          ],
+          onTap: onItemTapped,
+        ),
+      ),
+    );
+  }
+}
--- lib/telas/remedios/remedios_page.dart
+++ lib/telas/remedios/remedios_page.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:medic_app/componentes/remedio.dart';
 import 'package:medic_app/cores/color.dart';
+import 'package:medic_app/telas/calendario/calendario_page.dart';
 import 'package:medic_app/widgets/app_bar_standart.dart';
 import 'package:medic_app/widgets/remedio_screen_view.dart';
 
-class RemediosPage extends StatelessWidget {
+class RemediosPage extends StatefulWidget {
   const RemediosPage({super.key});
+
+  @override
+  State<RemediosPage> createState() => _RemediosPageState();
+}
+
+class _RemediosPageState extends State<RemediosPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const RemediosPage(), // Corpo da página de Remédios
+    const Placeholder(), // Placeholder para Home (você pode substituir pela sua tela Home)
+    const CalendarioPage(), // Página de Calendário
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => _pages[index],
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,21 +68,22 @@ class RemediosPage extends StatelessWidget {
           iconSize: 25,
           selectedItemColor: Cor.verdeIcones,
           elevation: 5,
-          currentIndex: 1,
+          currentIndex: _selectedIndex,
           items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
             BottomNavigationBarItem(
               icon: Icon(Icons.local_hospital),
               label: 'Medicacoes',
             ),
             BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
               icon: Icon(Icons.calendar_month),
               label: 'Calendario',
-            )
+            ),
           ],
+          onTap: _onItemTapped,
         ),
       ),
     );
